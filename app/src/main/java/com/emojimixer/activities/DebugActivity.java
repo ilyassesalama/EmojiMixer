36bd5bfc4b1d82a5fe75af8b0214c9fd5b4d82aa
Pranav <purwarpranav80@gmail.com>
2021-12-31 13:17:49 +0530

    switch to appcompat alert dialog

--- app/src/main/java/com/emojimixer/activities/DebugActivity.java
+++ app/src/main/java/com/emojimixer/activities/DebugActivity.java
@@ -1,7 +1,7 @@
 package com.emojimixer.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.app.AlertDialog;
+import androidx.appcompat.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
